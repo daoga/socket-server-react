e33af29a4c04fd442c09e9b8017f02e60063ea61
Diego Morales <die.moralesh@gmail.com>
2020-11-13 01:33:46 -0300

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,45 @@
+// Servidor de Express
+const express   = require('express');
+const http      = require('http');
+const socketio  = require('socket.io');
+const path      = require('path');
+const Sockets   = require('./socket');
+
+
+class Server {
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+
+        // Http server
+        this.server = http.createServer( this.app );
+
+        // Configuraciones de socket
+        this.io = socketio( this.server );
+    }
+
+    middlewares() {
+        // Desplegar el directorio publico
+        this.app.use( express.static( path.resolve( __dirname, '../public') ) );
+    }
+
+    configurarSockets() {
+        new Sockets( this.io );
+    }
+
+    execute() {
+
+        // Inicializar Middlewares
+        this.middlewares();
+
+        // Inicializar Socket
+        this.configurarSockets();
+
+        // Inicializar Server
+        this.server.listen( this.port , () => {
+            console.log('Server corriendo en puerto :', this.port);
+        });
+    }
+}
+
+module.exports = Server;
